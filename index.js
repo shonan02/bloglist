6a0faeaa22ab37c2be2ce76d2fb2604d15c9b8e6
shonan02 <66680460+shonan02@users.noreply.github.com>
2023-11-20 15:55:28 +0000

    reconfigure index.js

--- index.js
+++ index.js
@@ -1,42 +1,7 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
+const app = require('./app');
+const logger = require('./utils/logger');
+const config = require('./utils/config');
 
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-
-const blogSchema = new mongoose.Schema({
-    title: String,
-    author: String,
-    url: String,
-    likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema);
-
-const mongoUrl = `mongodb+srv://root:root@cluster1.sqbexiy.mongodb.net/Blog?retryWrites=true&w=majority`;
-
-mongoose.set('strictQuery', false);
-mongoose.connect(mongoUrl);
-
-app.get('/api/blogs', (req, res) => {
-    Blog.find({})
-        .then(blogs => res.json(blogs));
-})
-
-app.post('/api/blogs', (req,res) => {
-    const blog = new Blog(req.body);
-
-    blog.save().then(saved => {
-        res.status(201).json(saved);
-    })
-})
-
-
-const PORT = 3003;
-
-app.listen(PORT, () => {
-    console.log(`Server running on ${PORT}`);
+app.listen(config.PORT, () => {
+    logger.info(`Server running on ${config.PORT}`);
 })
